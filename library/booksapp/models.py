0c4247a8dd195e494dd7d8ac6c7a693a7ae4a2e1
Aureems <95098099+Aureems@users.noreply.github.com>
2022-07-16 14:01:47 +0300

    updated booksapp model

--- library/booksapp/models.py
+++ library/booksapp/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     category_name = models.CharField(max_length=100)
     subcategory_name = models.CharField(max_length=100, Blank=True)
 
+    class Meta:
+        verbose_name_plural = "List of Categories"
+
     def __str__(self):
         return str(f"{self.category_id} - {self.category_name}")
 
@@ -18,21 +21,27 @@ class Author(models.Model):
     author_id = models.AutoField(primary_key=True)
     author_first_name = models.CharField(max_length=100)
     author_last_name = models.CharField(max_length=100)
-    author_middle_name = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        verbose_name_plural = "List of Books"
 
     def __str__(self):
-        return str(f"{self.author_id} - {self.author_first_name}{' '+self.author_middle_name if not self.author_middle_name == '' else ''} {self.author_last_name}")
+        return str(f"{self.author_id} - {self.author_first_name} - {self.author_last_name}")
 
 
-class Books(models.Model):
+class Book(models.Model):
     isbn = models.CharField(primary_key=True, max_length=250)
     title = models.CharField(max_length=250)
-    author = models.ForeignKey(Author, null=True)
+    author = models.ForeignKey(Author, null=True, on_delete=models.CASCADE)
     description = models.CharField(max_length=10000)
     date_published = models.DateField()
     page_number = models.IntegerField(max_length=5)
     book_status = models.OneToOneField(Order, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='books/')
+
+    class Meta:
+        verbose_name_plural = "List of Books"
 
     def __str__(self):
         return str(f"{self.isbn} - {self.title}")
