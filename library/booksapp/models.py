88dea4aba1c5bb1424109f3cd8614b91d1477b42
Aureems <95098099+Aureems@users.noreply.github.com>
2022-07-16 13:21:22 +0300

    books model

--- library/booksapp/models.py
+++ library/booksapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    pass
+
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=100)
+    subcategory_name = models.CharField(max_length=100, Blank=True)
+
+    def __str__(self):
+        return str(f"{self.category_id} - {self.category_name}")
+
+
+class Author(models.Model):
+    author_id = models.AutoField(primary_key=True)
+    author_first_name = models.CharField(max_length=100)
+    author_last_name = models.CharField(max_length=100)
+    author_middle_name = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return str(f"{self.author_id} - {self.author_first_name}{' '+self.author_middle_name if not self.author_middle_name == '' else ''} {self.author_last_name}")
+
+
+class Books(models.Model):
+    isbn = models.CharField(primary_key=True, max_length=250)
+    title = models.CharField(max_length=250)
+    author = models.ForeignKey(Author, null=True)
+    description = models.CharField(max_length=10000)
+    date_published = models.DateField()
+    page_number = models.IntegerField(max_length=5)
+    book_status = models.OneToOneField(Order, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(f"{self.isbn} - {self.title}")
--- library/library/settings.py
+++ library/library/settings.py
@@ -134,9 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = BASE_DIR, 'static/'
 MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_ROOT = BASE_DIR, 'images/'
 
 
 # Default primary key field type
