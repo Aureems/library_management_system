52ac70f216dc61644003aa21b2473cd3a9cf43e2
Aureems <95098099+Aureems@users.noreply.github.com>
2022-08-11 23:07:49 +0300

    fix for heroku3

--- library/library/settings.py
+++ library/library/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-s%-r+u00m02swhsdmj)-s8cc+%padr#k%ll7fl@%yn%qr3o_#4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bookworm-library-project.herokuapp.com/']
+ALLOWED_HOSTS = ['https://bookworm-library-project.herokuapp.com/']
 
 
 # Application definition
