b69506bc2a137f26a6cf800d1efdbbab6b070eec
Aureems <95098099+Aureems@users.noreply.github.com>
2022-08-11 23:02:13 +0300

    fix for heroku2

--- library/library/settings.py
+++ library/library/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-s%-r+u00m02swhsdmj)-s8cc+%padr#k%ll7fl@%yn%qr3o_#4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bookworm-library-project.herokuapp.com/']
 
 
 # Application definition
