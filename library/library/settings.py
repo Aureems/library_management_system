0a64a765e83c139fef5a7c24769f185373370c57
Aureems <95098099+Aureems@users.noreply.github.com>
2022-08-12 21:25:36 +0300

    Remove a hardcoded Django secret key

--- library/library/settings.py
+++ library/library/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s%-r+u00m02swhsdmj)-s8cc+%padr#k%ll7fl@%yn%qr3o_#4'
+# SECRET_KEY = 'django-insecure-s%-r+u00m02swhsdmj)-s8cc+%padr#k%ll7fl@%yn%qr3o_#4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://bookworm-library-project.herokuapp.com/']
+ALLOWED_HOSTS = ['url']
 
 
 
