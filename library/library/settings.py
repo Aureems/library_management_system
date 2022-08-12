c93ad73fc3de54d144cb7c02a04c6316e55cd4c7
Aureems <95098099+Aureems@users.noreply.github.com>
2022-08-12 22:22:43 +0300

    maybe now

--- library/library/settings.py
+++ library/library/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['url']
+ALLOWED_HOSTS = ['https://bookworm-library-project.herokuapp.com/']
 
 
 
